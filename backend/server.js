ce969f5e1847f51a4a7e9c8e1c5a4f47f13412e2
crownwcg <crownwcg@hotmail.com>
2019-11-23 17:47:38 -0800

    change server port

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('COMING SOON'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
