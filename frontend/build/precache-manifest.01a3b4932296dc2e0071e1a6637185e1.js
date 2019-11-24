ad5c8ffe0c097e568c415604904606eb433ca209
crownwcg <crownwcg@hotmail.com>
2019-11-23 16:34:30 -0800

    front end deployment with github page

---
+++ frontend/build/precache-manifest.01a3b4932296dc2e0071e1a6637185e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34fb1ee63d608f6a3f1ac6b673c66e17",
+    "url": "/overseae/index.html"
+  },
+  {
+    "revision": "2673f17cf5a17166d8e3",
+    "url": "/overseae/static/css/main.85e25a77.chunk.css"
+  },
+  {
+    "revision": "967f5baced8b3e330c15",
+    "url": "/overseae/static/js/2.d927b1f7.chunk.js"
+  },
+  {
+    "revision": "2673f17cf5a17166d8e3",
+    "url": "/overseae/static/js/main.076ec11a.chunk.js"
+  },
+  {
+    "revision": "c6b5fa6426b8b2e3edfe",
+    "url": "/overseae/static/js/runtime-main.ecbfd8e0.js"
+  }
+]);
